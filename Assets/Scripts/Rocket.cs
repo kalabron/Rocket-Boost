738d92041c3379d058ea058d2f7192707a3ae6b4
kalabron <kalabron@gmail.com>
2019-10-19 12:42:20 -0400

    added scene transitions for level 1 and 2

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
@@ -10,6 +11,9 @@ public class Rocket : MonoBehaviour
 
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
@@ -51,17 +55,28 @@ public class Rocket : MonoBehaviour
         {
             case "Friendly":
                 // do nothing
-                print("okay"); //todo remove
                 break;
-            case "Fuel":
-                print("Fuel"); //todo remove
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadNextScene", 1f);  // parameterise time
                 break;
             default:
-                print("Dead");
-                // kill player
+                state = State.Dying;
+                Invoke("LoadFirstScene", 1f);  // parameterise time
                 break;
         }
     }
+
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);  //todo allow for more than 2 levels
+    }
+
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
